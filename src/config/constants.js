2dc4c4ce81fe2aa2d8852a2f8ef21c1174335936
JefferDevop <somadesarrollo@gmail.com>
2025-04-21 15:20:33 -0500

    Change hosting conexion.bar

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,5 @@
 
- export const BASE_API = `https://variedadesel184.catalogointeractivo.com.co`;
+ export const BASE_API = `https://variedadesel184.conexion.bar`;
 
 export const SEARCH_HISTORY = "search-history";
 export const CART = "cart";
