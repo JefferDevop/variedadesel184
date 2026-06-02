509fb12eae15ed8595e007f4654bc87bcf64e64f
JefferDevop <77632174+JefferDevop@users.noreply.github.com>
2026-06-02 04:58:42 -0500

    Update BASE_API URL for the application

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,5 @@
 
- export const BASE_API = `https://variedadesel184.conexion.bar`;
+ export const BASE_API = `https://variedadesel184.cosmoqa.store`;
 
 export const SEARCH_HISTORY = "search-history";
 export const CART = "cart";
